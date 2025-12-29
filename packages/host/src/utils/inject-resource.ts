58994748939ccf2316dd12fdca0b0728630880e4
witsaint <gao973738@qq.com>
2025-12-29 23:17:40 +0800

    refactor: update injectResource function and enhance BundleHtmlOptions interface

--- packages/host/src/utils/inject-resource.ts
+++ packages/host/src/utils/inject-resource.ts
@@ -2,19 +2,26 @@ import type { Uri, Webview } from 'vscode'
 import { getNonce } from './get-nonce'
 import { getUri } from './get-uri'
 
-interface IInjectLinkSrc {
+export interface IInjectResource {
   src: string
-  integrity: string
-  crossOrigin: string
+  sourcePath: string
 }
-export function injectResource(htmlStr: string, paths: IInjectLinkSrc[], webview: Webview, extensionUri: Uri): string {
+/**
+ *
+ * @param htmlStr - The HTML string to inject resources into.
+ * @param paths - The paths to inject resources from.
+ * @param webview - The webview to inject resources into.
+ * @param extensionUri - The extension URI to inject resources from.
+ * @returns The HTML string with resources injected.
+ */
+export function injectResource(htmlStr: string, paths: IInjectResource[], webview: Webview, extensionUri: Uri): string {
   const nonce = getNonce()
   let templateStr: string = htmlStr
     .replace(/\{\{nonce\}\}/g, nonce)
     .replace(/<script /g, `<script nonce="${nonce}" `)
 
   for (const path of paths) {
-    const uri = getUri(webview, extensionUri, [path.src])
+    const uri = getUri(webview, extensionUri, [path.sourcePath])
     templateStr = templateStr.replace(
       new RegExp(`${path.src}(\\?\\d+)?`),
       uri.toString(),
--- packages/host/src/webview/vs-html.ts
+++ packages/host/src/webview/vs-html.ts
@@ -1,3 +1,8 @@
+import type { Webview } from 'vscode'
+import * as fs from 'node:fs'
+import { Uri } from 'vscode'
+import { type IInjectResource, injectResource } from '../utils/inject-resource'
+
 type ServerUrl = URL | string | {
   protocol: string
   host: string
@@ -6,8 +11,10 @@ type ServerUrl = URL | string | {
 }
 
 interface BundleHtmlOptions {
-  dirPath: string
-  htmlName: string
+  extensionUri: Uri
+  webview: Webview
+  htmlPath: string
+  resources: IInjectResource[]
 }
 /**
  *
@@ -29,7 +36,24 @@ export function getHtmlByServer(url: ServerUrl, opts: RequestInit = {}): Promise
   return fetch(_url, opts).then(result => result.text())
 }
 
-export function getHtmlByBundle(opts: BundleHtmlOptions): Promise<string> {
-  const { dirPath } = opts
-  return Promise.resolve(dirPath)
+/**
+ *
+ * @param opts - The options to pass to the get HTML by bundle request.
+ * @returns The HTML as a string.
+ */
+export function getHtmlByBundle(opts: BundleHtmlOptions): Promise<string> | string {
+  const { extensionUri, webview, htmlPath, resources } = opts
+  try {
+    const htmlStr = fs.readFileSync(
+      Uri.joinPath(extensionUri, htmlPath).fsPath,
+      'utf-8',
+    )
+    if (resources.length > 0) {
+      return injectResource(htmlStr, resources, webview, extensionUri)
+    }
+    return Promise.resolve(htmlStr)
+  }
+  catch (error) {
+    return Promise.reject(error)
+  }
 }
